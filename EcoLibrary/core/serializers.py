aa4dc1d981d6e0db5b47a899a70c7426dc55fb30
Frank Soto <fsotoa@usm.cl>
2025-11-30 19:04:57 -0300

    first commit

---
+++ EcoLibrary/core/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import Libro
+
+class LibroSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Libro
+        fields = ['id', 'titulo', 'autor', 'descripcion', 'categoria', 'anio_publicacion', 'imagen_url']
+
+class LibroDetalleSerializer(LibroSerializer):
+    informacion_complementaria = serializers.SerializerMethodField() 
+
+    class Meta(LibroSerializer.Meta):
+        fields = LibroSerializer.Meta.fields + ['informacion_complementaria']
+        
+    def get_informacion_complementaria(self, obj):
+        from .utils import obtener_datos_api_externa 
+
+        try:
+            return obtener_datos_api_externa(obj.titulo)
+        except Exception:
+            return "Información complementaria no disponible"
+
---
+++ EcoLibrary/core/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path, include
+from . import views
+from rest_framework.routers import DefaultRouter
+from django.contrib.auth import views as auth_views
+
+router = DefaultRouter()
+router.register(r'libros', views.LibroViewSet)
+
+urlpatterns = [
+
+    path('registro/', views.registro_view, name='registro'), 
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    
+    path('', views.LibroListView.as_view(), name='lista_libros'),
+    path('libro/<int:pk>/', views.LibroDetailView.as_view(), name='detalle_libro'), 
+
+    path('favoritos/', views.FavoritoListView.as_view(), name='lista_favoritos'), 
+    path('libro/<int:libro_id>/marcar_favorito/', views.marcar_favorito_view, name='marcar_favorito'), 
+    path('favorito/<int:pk>/eliminar/', views.FavoritoDeleteView.as_view(), name='eliminar_favorito'), 
+
+    path('api/v1/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+]
